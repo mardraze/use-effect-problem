5c78a96cc4349de81478a71d0b052289aa7f3af9
Marcin Drazek <marcindr1@gmail.com>
2024-01-29 20:42:37 +0100

    Update variable from user input and custom event

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [log, setLog] = useState([]);
+
+  useEffect(() => {
+    const onCustomEvent = function(){
+      setCount(count+1)
+    }
+    logEvent('addEventListener')
+    document.addEventListener('custom-event', onCustomEvent)
+
+    return () => {
+      logEvent('removeEventListener')
+      document.removeEventListener('custom-event', onCustomEvent)
+    }
+  }, [count, setCount])
+
+  function dispatchEvent(){
+    document.dispatchEvent(new CustomEvent('custom-event'))
+  }
+
+  function logEvent(msg){
+    console.log(msg)
+    setLog([...log, msg])
+  }
+
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>
+        Increase counter
+      </button>
+      <button onClick={() => dispatchEvent()}>
+        Dispatch a Custom Event, for example from a WebSockets
+      </button>
+      <div>I need to update a variable, for example counter, from buttons and from WebSockets.
+         How to do it in react? Currently I use "useEffect" hook, but I don't think it's a correct way, 
+         because I'd like to avoid addEventListener and removeEventListener on every counter change.
+        Does anyone know a solution?</div>
+      <div>count value is {count}</div>
+      <div>Log (see console also):</div>
+      {log.map((msg, i) => <div key={i}>{msg}</div>)}
+    </>
+  );
+}
+
+export default App;
